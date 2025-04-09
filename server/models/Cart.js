6744c8b3a9d71b31f87483aa7f554095bbb38500
Mohammed Yasser <2100921@eng.asu.edu.eg>
2025-04-09 15:54:22 +0200

    Add Cart model with schemas for cart items and user cart

---
+++ server/models/Cart.js
@@ -0,0 +1,17 @@
+// models/Cart.js
+const mongoose = require('mongoose');
+
+const cartItemSchema = new mongoose.Schema({
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+  quantity: { type: Number, default: 1 },
+  selectedColor: String,
+  selectedSize: String
+});
+
+const cartSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  items: [cartItemSchema],
+  updatedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Cart', cartSchema);
