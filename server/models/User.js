41f341de0bbb824b88fbe2089880dc4706812951
Mohammed Yasser <2100921@eng.asu.edu.eg>
2025-04-09 15:55:44 +0200

    Add User model with schema for user details and password hashing

--- server/models/User.js
+++ server/models/User.js
@@ -1,2 +1,50 @@
-// server/models/User.js
-// You can use Mongoose or Sequelize here
+// models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+
+  profilePic: {
+    type: String,
+    default: ''
+  },
+
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  },
+
+  address: {
+    country: String,
+    city: String,
+    zip: String,
+    street: String
+  },
+
+  createdAt: { type: Date, default: Date.now }
+});
+
+// Password hashing middleware
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
