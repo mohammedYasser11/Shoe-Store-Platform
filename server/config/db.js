1380fd5cda3d31ab7ed18ff3ccd611fa94c16509
Mohammed Yasser <2100921@eng.asu.edu.eg>
2025-04-09 15:25:02 +0200

    Set up database connection and environment variables

--- server/config/db.js
+++ server/config/db.js
@@ -1,2 +1,13 @@
-// server/config/db.js
-// Use mongoose.connect() or Sequelize setup
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to DB: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-// server/server.js
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config();
+connectDB(); // <-- connect to DB
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
