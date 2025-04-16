f2b9f7e10ed692131a356f2e5fc55b1d04c345a2
Mohammed Yasser <2100921@eng.asu.edu.eg>
2025-04-17 00:59:27 +0200

    Add multer middleware for profile picture upload in user profile route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,8 @@
 const router = require('express').Router();
 const { updateProfile } = require('../controllers/userController');
 const { protect } = require('../middleware/auth');
+const upload = require('../middleware/multer');
 
-router.put('/me', protect, updateProfile);
+router.put('/me', protect, upload.single('profilePicture'), updateProfile);
 
 module.exports = router;
