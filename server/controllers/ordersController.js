bcc11f4ec90e4698325e5420d4c331d130e54654
Mohammed Yasser <2100921@eng.asu.edu.eg>
2025-04-19 15:36:01 +0200

    Fix API endpoint documentation for fetching user orders

--- server/controllers/ordersController.js
+++ server/controllers/ordersController.js
@@ -1,14 +1,13 @@
 const Order = require('../models/Order');
 
 /**
- * GET /api/orders
+ * GET /api/order
  * Return all orders for the currently authenticated user.
  */
 exports.getUserOrders = async (req, res) => {
     try {
         // req.user should contain the user ID from your auth middleware
         const userId = req.user.id || req.user._id;
-
         // Fetch orders, newest first, and populate each item's product name
         const orders = await Order.find({ userId })
         .sort({ orderedAt: -1 })
