ae6bdc0e8a20d862b8945d1fd73f232403918a46
Mohammed Yasser <2100921@eng.asu.edu.eg>
2025-05-05 01:46:56 +0300

    Refactor admin routes: Improve code formatting and consistency for better readability

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,34 +1,46 @@
-const express = require('express');
-const { protect, adminOnly } = require('../middleware/auth');
+const express = require("express");
+const { protect, adminOnly } = require("../middleware/auth");
 const {
-  getAllUsers, getUserById, createUser, updateUser, deleteUser
-} = require('../controllers/usersController');
-const { getAllOrders, updateOrderStatus } = require('../controllers/orderController');
-const { getDashboardData }    = require('../controllers/adminController');
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/usersController");
+const {
+  getAllOrders,
+  updateOrderStatus,
+} = require("../controllers/orderController");
+const { getDashboardData } = require("../controllers/adminController");
 const {
   updateVariantStock,
-  updateProductDiscount
-} = require('../controllers/productController');
+  updateProductDiscount,
+  updateProductVariantDiscount,
+} = require("../controllers/productController");
 const router = express.Router();
 
 // All /api/* require an admin token
 router.use(protect, adminOnly);
 
 // Dashboard stats
-router.get('/dashboard', getDashboardData);
+router.get("/dashboard", getDashboardData);
 // User management
-router.get ('/users',       getAllUsers);
-router.get ('/users/:id',   getUserById);
-router.post('/users',       createUser);
-router.put ('/users/:id',   updateUser);
-router.delete('/users/:id', deleteUser);
+router.get("/users", getAllUsers);
+router.get("/users/:id", getUserById);
+router.post("/users", createUser);
+router.put("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
 
 // Order management
-router.get ('/orders',         getAllOrders);
-router.put ('/orders/:id',     updateOrderStatus);
+router.get("/orders", getAllOrders);
+router.put("/orders/:id", updateOrderStatus);
 
 // Inventory management
-router.put('/products/:productId/variants/:variantId', updateVariantStock); // Add the route here
-router.put('/products/:productId/discount',     updateProductDiscount);
+router.put("/products/:productId/variants/:variantId", updateVariantStock);
+router.put("/products/:productId/discount", updateProductDiscount);
+router.put(
+  "/products/:productId/discount/:variantId",
+  updateProductVariantDiscount
+);
 
 module.exports = router;
